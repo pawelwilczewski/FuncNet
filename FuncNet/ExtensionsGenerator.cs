590419d658d42f9d6954305251fcc3a95b60d056
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-05-27 01:23:15 +0100

    ExtensionsGenerator cleanup

--- FuncNet/ExtensionsGenerator.cs
+++ FuncNet/ExtensionsGenerator.cs
@@ -369,25 +369,6 @@ public static class OptionMatch
 		};
 	}
 }");
-
-		context.AddSource("Pipe", @"
-using System;
-using System.Threading.Tasks;
-
-namespace FuncNet;
-
-public static class PipeExtensions
-{
-	public static TNew Pipe<TOld, TNew>(this TOld value, Func<TOld, TNew> func) => func(value);
-
-	public static async Task<TNew> Pipe<TOld, TNew>(this Task<TOld> value, Func<TOld, TNew> func) =>
-		func(await value.ConfigureAwait(false));
-
-	public static async Task<TNew> Pipe<TOld, TNew>(this Task<TOld> value, Func<TOld, Task<TNew>> func) =>
-		await func(await value.ConfigureAwait(false)).ConfigureAwait(false);
-}");
-
-		// Console.WriteLine($"Generated in {Stopwatch.GetElapsedTime(startTime)}");
 	}
 
 	private static string GenerateSourceFile(UnionExtensionsFileGenerationParams p) =>
